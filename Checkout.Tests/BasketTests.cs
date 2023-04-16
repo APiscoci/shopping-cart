3253ca07c981c09047551313982b11385f12f404
APiscoci <piscoci.anca@gmail.com>
2023-04-16 14:54:20 +0100

    test: add final test

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -22,7 +22,6 @@ public class BasketTests
 		
 		// Then
 		Assert.Equal(0, total);
-
 	}
 	
 	[Theory]
@@ -40,7 +39,6 @@ public class BasketTests
 		// Then 
 		Assert.Equal(expectedPrice, total);
 	}
-	
 	
 	[Theory]
 	[InlineData(1, 50)]
@@ -67,7 +65,6 @@ public class BasketTests
 	[InlineData(4, 55)]
 	[InlineData(5, 70)]
 	[InlineData(6, 80)]
-	
 	public void ShouldApplyQuantityPromotionToProductB(int quantity, decimal expectedTotal)
 	{
 		// Given
@@ -80,14 +77,13 @@ public class BasketTests
 		Assert.Equal(total, expectedTotal);
 	}
 	
-		[Theory]
+	[Theory]
 	[InlineData(1, 55)]
 	[InlineData(2, 82.5)]
 	[InlineData(3, 137.5)]
 	[InlineData(4, 165)]
 	[InlineData(5, 220)]
 	[InlineData(6, 247.5)]
-	
 	public void ShouldApplyQuantityPromotionToProductD(int quantity, decimal expectedTotal)
 	{
 		// Given
@@ -98,5 +94,25 @@ public class BasketTests
 
 		// Then
 		Assert.Equal(total, expectedTotal);
+	}
+	
+	[Fact]
+	public void ShouldAddAllProductsToBasket ()
+	{
+		// Given
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "A" , UnitPrice = 10m, QuantityPerProduct = 1 });
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "B" , UnitPrice = 15m, QuantityPerProduct = 3 });
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "C" , UnitPrice = 40m, QuantityPerProduct = 2 });
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "D" , UnitPrice = 55m, QuantityPerProduct = 3 });
+		
+			// When 
+		var total = _basket.GetTotalBasketCost();
+		
+		// Then 
+		Assert.Equal(267.5m, total);
 	}
 }
