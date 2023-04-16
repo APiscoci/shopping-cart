4a793a8ae00924e503f379c69ddc798fc33040d8
APiscoci <piscoci.anca@gmail.com>
2023-04-16 13:37:04 +0100

    test: refactor and add test case for quantity promotion

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -18,21 +18,58 @@ public class BasketTests
 		var total = _basket.GetTotalBasketCost();
 		
 		// Then
-		Assert.Equal(total, 0);
+		Assert.Equal(0, total);
 
 	}
 	
-	[Fact]
-	public void ShouldAddProductsToBasket()
+	[Theory]
+	[InlineData(1, 10)]
+	[InlineData(2, 20)]
+	public void ShouldAddProductsToBasket(int quantity, decimal expectedPrice)
 	{
 		// Given
 		_basket.AddProductToBasket(
-			new BasketProduct{StockKeepingUnit = "A" , UnitPrice = 10m, QuantityPerProduct = 1 });
+			new BasketProduct{StockKeepingUnit = "A" , UnitPrice = 10m, QuantityPerProduct = quantity });
 		
 		// When 
 		var total = _basket.GetTotalBasketCost();
 		
 		// Then 
-		Assert.Equal(total, 10);
+		Assert.Equal(expectedPrice, total);
 	}
+	
+	
+	[Theory]
+	[InlineData(1, 50)]
+	[InlineData(2, 100)]
+	public void ShouldAddDiferentProductsToBasket (int quantity, decimal expectedPrice)
+	{
+		// Given
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "A" , UnitPrice = 10m, QuantityPerProduct = quantity });
+		_basket.AddProductToBasket(
+			new BasketProduct{StockKeepingUnit = "C" , UnitPrice = 40m, QuantityPerProduct = quantity });
+		
+			// When 
+		var total = _basket.GetTotalBasketCost();
+		
+		// Then 
+		Assert.Equal(expectedPrice, total);
+	}
+	
+	
+	[Fact]
+	public void SHouldApplyQuantityPromotionToItemB()
+	{
+		// Given
+		_basket.AddProductToBasket(new BasketProduct{StockKeepingUnit = "B", UnitPrice = 15m, QuantityPerProduct = 3});
+
+		// When
+		var total = _basket.GetTotalBasketCost();
+
+		// Then
+		Assert.Equal(40, total);
+	}
+	
+	
 }
